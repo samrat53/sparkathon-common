0358fd4011ce4d414afa030f497f2f0f56f16bed
samrat53 <samrat05hccs@gmail.com>
2024-08-12 04:01:05 +0530

    build stores'/get-requests/:storeId that returns the all the recent requests

--- common-backend/src/routes/store.ts
+++ common-backend/src/routes/store.ts
@@ -1,9 +1,35 @@
 import express from "express";
-const router= express.Router();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const router = express.Router();
 // all routes that serve the web of customer service
 
-router.get("/login",(req,res)=>{
-    res.send("hello from /api/v1/store/login")
-})
+router.get("/get-requests/:storeId", async (req, res) => {
+  const storeId = Number(req.params.storeId);
+  try {
+    const allRequests = await prisma.customerRequests.findMany({
+      where: {
+        storeId: storeId,
+      },
+      select: {
+        requestId: true,
+        customer_name: true,
+        time: true,
+        itemId: true,
+        phone: true,
+      },
+      orderBy: {
+        time: "desc",
+      },
+    });
+    res.status(200).json({
+      message: "got new request",
+      allRequests: allRequests,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 export default router;
